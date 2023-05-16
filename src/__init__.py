356c59faef9fb39b876e31000e8d515ebed1c6e8
ardammr <arda.mamur@voids.ai>
2023-05-16 23:49:27 +0200

    v1.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from utils._prepare_data import DataHandler
+from utils._get_model import generate_model
 from dataset.VerSe import VerSe
-from models import DenseNet3D, UNet3D, ResNet3D
 
-_all_ = [ VerSe, DataHandler, DenseNet3D, UNet3D, ResNet3D]
+_all_ = [ VerSe, generate_model, DataHandler]
