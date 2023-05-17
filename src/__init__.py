89a960dea5609472444df7b26499bb58d461dff8
ardammr <arda.mamur@voids.ai>
2023-05-17 03:03:18 +0200

    v1.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,11 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from utils._prepare_data import DataHandler
 from utils._get_model import generate_model
+from utils.settings import parse_opts
 from dataset.VerSe import VerSe
+from models.ResNet3D import ResNet
+from models.UNet3D import UNet3D
+from modules.ResNetModule import ResNetLightning
+from modules.VerSeDataModule import VerSeDataModule
 
-_all_ = [ VerSe, generate_model, DataHandler]
+_all_ = [ VerSe, generate_model, DataHandler, ResNet, UNet3D, ResNetLightning, VerSeDataModule, parse_opts]
