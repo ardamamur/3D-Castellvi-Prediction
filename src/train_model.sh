830a60ac8a7aea8bfd9d35dc67fa1dc99cca7b0b
melisaankut <melisankut@gmail.com>
2023-05-19 18:06:03 +0200

    changes path

--- src/train_model.sh
+++ src/train_model.sh
@@ -6,4 +6,4 @@ export CUDA_VISIBLE_DEVICES=3
 export PYTHONPATH=$PYTHONPATH:/data1/practical-sose23/castellvi/castellvi_prediction/bids
 export PYTHONWARNINGS="ignore"
 
-python3 train.py --settings /u/home/ank/3D-Castellvi-Prediction/settings.yaml
+python3 train.py --settings /u/home/mamar/3D-Castellvi-Prediction/settings.yaml
