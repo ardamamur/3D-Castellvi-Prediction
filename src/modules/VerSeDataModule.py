a322eac9d0ab18d5d23882c5c83e21e60624bdb3
Daniel-Jordi Regenbrecht <daniel-jordi.regenbrecht@tum.de>
2023-05-30 16:46:54 +0200

    added weighted random sampling for training

--- src/modules/VerSeDataModule.py
+++ src/modules/VerSeDataModule.py
@@ -3,6 +3,8 @@ import pandas as pd
 from utils._prepare_data import DataHandler
 from dataset.VerSe import VerSe
 from torch.utils.data import DataLoader
+import torch
+
 
 class VerSeDataModule(pl.LightningDataModule):
 
@@ -40,12 +42,45 @@ class VerSeDataModule(pl.LightningDataModule):
         if stage in {'test', None}:
             self.train_dataset = VerSe(self.opt, self.processor, self.test_records, training=False)
 
+    def _get_sampler(self, dataset):
+        # Compute the true labels for each sample
+        true_labels = []
+        for index in range(len(dataset)):
+            true_labels.append(dataset.__getitem__(index)["class"])
+
+        true_labels = torch.tensor(true_labels)
+
+        # Count the occurrences of each true label
+        label_counts = torch.bincount(true_labels)
+
+        # Compute the inverse of the label counts to get the weights
+        weights = 1.0 / label_counts.float()
+
+        # Normalize the weights to sum up to the number of classes
+        weights = weights / weights.sum() * len(label_counts)
+
+        weights = weights.tolist()
+
+        sampler = torch.utils.data.WeightedRandomSampler(weights, len(dataset))
+        return sampler
+
+
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+        dataset = VerSe(self.processor, self.castellvi_classes, self.pad_size, self.use_seg, self.use_binary_classes, training=True)
+        train_dataset, _ = random_split(dataset, self.train_val_split, generator = torch.Generator().manual_seed(42))
+
+        sampler = self._getsampler(train_dataset)
+        return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, sampler = sampler)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        #TODO: Does weighted sampling make sense for validation too?
+        dataset = VerSe(self.processor, self.castellvi_classes, self.pad_size, self.use_seg, self.use_binary_classes, training=True)
+        _ , val_dataset = random_split(dataset, self.train_val_split, generator = torch.Generator().manual_seed(42))
 
+        
+        return DataLoader(val_dataset, batch_size=self.batch_size)
+"""
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size)
 
