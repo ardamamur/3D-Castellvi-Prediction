4cb1f7175ec7d0487531137b4ad6d551e00879ca
Daniel-Jordi Regenbrecht <daniel-jordi.regenbrecht@tum.de>
2023-07-08 16:01:42 +0200

    Implemented angle related experiments and new preprocessing

--- src/utils/_prepare_data.py
+++ src/utils/_prepare_data.py
@@ -2,15 +2,17 @@ import logging
 import numpy as np
 import pandas as pd
 import BIDS
-from BIDS import BIDS_Global_info, BIDS_Family, NII
-from BIDS.snapshot2D import create_snapshot,Snapshot_Frame,Visualization_Type,Image_Modes
-from pathlib import Path
+from BIDS import BIDS_Global_info
+from BIDS.centroids import load_centroids
 import types
 from tqdm import tqdm
 import yaml
-from pqdm.threads import pqdm
+from pqdm.processes import pqdm
 import logging
 import os
+import random
+import nibabel as nib
+import sys
 
 from functools import partial
 
@@ -147,7 +149,7 @@ class DataHandler:
         return record
         
     
-    def _compute_slice(self, seg_arr:np.ndarray, ctd:BIDS.Centroids, max_shape:tuple | None):
+    def _compute_slice(self, seg_arr:np.ndarray, ctd:BIDS.Centroids, max_shape:tuple | None, zoom:tuple):
         """
         Args: 
             Segmentation Array, Max Shape, BIDS Centroids file
@@ -156,6 +158,9 @@ class DataHandler:
             the lowest L vertebra and the image part up to the centroid of the superior vertebra and down to the S1 centroid.
             Slices are increased to max_shape dimension-wise to include as much real image data as possible.
         """
+
+        #adjust the max_shape to work with general zooms
+        max_shape = tuple([int(max_shape[i] / zoom[i]) for i in range(3)]) if max_shape else None
 
         lowest_L_idx = 25 if 25 in ctd else 24 if 24 in ctd else 23 if 23 in ctd else None
         assert(lowest_L_idx != None)
@@ -188,10 +193,15 @@ class DataHandler:
             assert(lr_slice.stop-lr_slice.start == max_shape[2])
             assert(is_slice.stop-is_slice.start == max_shape[1])
 
+        #Replace negative values with 0
+        ap_slice = slice(max(ap_slice.start, 0), max(ap_slice.stop, 0))
+        lr_slice = slice(max(lr_slice.start, 0), max(lr_slice.stop, 0))
+        is_slice = slice(max(is_slice.start, 0), max(is_slice.stop, 0))
+
         return ap_slice, lr_slice, is_slice
 
 
-    def _get_cutout(self, record, return_seg, max_shape, save_dir = "/data1/practical-sose23/castellvi/3D-Castellvi-Prediction/data", skip_existing = True):
+    def _get_cutout(self, record, return_seg, max_shape, save_dir = "/home/daniel/Documents/Uni/practical-sose23/castellvi/3D-Castellvi-Prediction/data", skip_existing = True):
         """
         Args:
             BIDS Family, return_seg (instead of ct), max_shape
@@ -202,78 +212,144 @@ class DataHandler:
         """
         assert(save_dir != None)
 
-        dir_path = save_dir + "/cutouts/shape_{}_{}_{}".format(max_shape[0], max_shape[1], max_shape[2])
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
         filepath_seg = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "seg")
         filepath_ct = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "ct")
 
         if os.path.isfile(filepath_seg + ".npy") and os.path.isfile(filepath_ct + ".npy") and skip_existing:
-            #logging.info("Skipping existing cutouts of subject {}".format(record["subject"]))
             if return_seg:
                 return(np.load(file=filepath_seg + ".npy"))
             else:
                 return(np.load(file=filepath_ct + ".npy"))
 
-        seg_nii = record["seg_file"].open_nii()
-        ctd = record["ctd_file"].open_cdt()
-        ctd.zoom = seg_nii.zoom
-
-        
-        ct_nii = record["raw_file"].open_nii()
-
-        seg_nii.reorient_(axcodes_to=('P', 'I', 'R'), verbose = False)
-        ctd.reorient_(axcodes_to=('P', 'I', 'R'), _shape = seg_nii.shape, verbose = False)
-
-        
-        ct_nii.reorient_(axcodes_to=('P', 'I', 'R'), verbose = False)
-
-
-        seg_nii.rescale_(voxel_spacing = (1,1,1))
-        ctd.rescale_(voxel_spacing = (1,1,1))
-
-    
-        ct_nii.rescale_(voxel_spacing = (1,1,1))
-
-
-        seg_arr = seg_nii.get_array()
-
-        try:
-            ap_slice, lr_slice, is_slice = self._compute_slice(seg_arr = seg_arr, ctd = ctd, max_shape = max_shape)
-        except Exception:
-            raise Exception("")
-        
-        seg_cutout = seg_arr[ap_slice, is_slice, lr_slice]
-        #We still need to pad in case the slice exceeded the image bounds i.e. no image data is available for the entire range
-        seg_cutout = np.pad(seg_cutout, [(0, max_shape[i] - seg_cutout.shape[i]) for i in range(len(seg_cutout.shape))],"constant")
-        np.save(filepath_seg, arr = seg_cutout)
-        logging.info("Saved new seg file {}".format(filepath_seg))
-            
-        
-        ct_arr = ct_nii.get_array()
-        ct_cutout = ct_arr[ap_slice, is_slice, lr_slice]
-        #We still need to pad in case the slice exceeded the image bounds i.e. no image data is available for the entire range
-        ct_cutout = np.pad(ct_cutout, [(0, max_shape[i] - ct_cutout.shape[i]) for i in range(len(ct_cutout.shape))],"constant")
-        np.save(filepath_ct, arr = ct_cutout)
-        logging.info("Saved new CT file {}".format(filepath_ct))
+        else:
+            self._prepare_cutout(record, max_shape, save_dir = save_dir, skip_existing = skip_existing)
         
         if return_seg:
             return(np.load(file=filepath_seg + ".npy"))
         else:
             return(np.load(file=filepath_ct + ".npy"))
+        
+    def get_ct_seg_ctd_cutout(self, record, max_shape=(128, 86, 136), save_dir = "/home/daniel/Documents/Uni/practical-sose23/castellvi/3D-Castellvi-Prediction/data", skip_existing = True):
+        """
+        Args:
+            BIDS Family, max_shape
+        Returns:
+            Cutout for the given subject.
+            Cutouts generally contains the full lowest vertebra and are always padded to max_shape
+    
+        """
+        assert save_dir != None
 
-    def _prepare_cutouts(self, save_dir, max_shape=(128, 86, 136), n_jobs = 8):
+        filepath_ctd = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "ctd")
+        filepath_seg = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "seg")
+        filepath_ct = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "ct")
+        return self._np_to_nifti(np.load(file=filepath_ct + ".npy")), self._np_to_nifti(np.load(file=filepath_seg + ".npy")), load_centroids(filepath_ctd + ".json", verbose = False)
+
+    def _prepare_cutouts(self, save_dir, max_shape=(128, 86, 136), n_jobs = 8, skip_existing = True):
 
         assert(save_dir != None)
         total_records = self.verse_records + self.tri_records
-        seg_fun = partial(self._get_cutout, return_seg = False, max_shape = max_shape, save_dir = save_dir, skip_existing = True)
-        res = pqdm(total_records, seg_fun, n_jobs = n_jobs)
-        for r in res:
-            print(r)
+        seg_fun = partial(self._prepare_cutout, max_shape = max_shape, save_dir = save_dir, skip_existing = skip_existing)
+        random.shuffle(total_records)
+        
+        #We use pqdm to apply seg_fun to all records in parallel
+        results = pqdm(total_records, seg_fun, n_jobs=n_jobs)
+        for result in results:
+            print(result)
+
+    def _np_to_nifti(self, np_arr):
+        #Assumes that the array is in (P, I, R) orientation
+        #define affine matrix to change (PIL) coordinates to (RAS) coordinates
+        affine = np.array([[ 0,  0,  1,  0],
+            [-1,  0,  0,  0],
+            [ 0, -1,  0,  0],
+            [ 0,  0,  0,  1]])
+        return(BIDS.NII(nib.Nifti1Image(np_arr, affine)))
+        
         
 
+    def _prepare_cutout(self, record, max_shape, save_dir = "/home/daniel/Documents/Uni/practical-sose23/castellvi/3D-Castellvi-Prediction/data", skip_existing = True):
+        """
+        Args:
+            record, max_shape
+        """
+
+        #Check for existing cutouts
+        assert(save_dir != None)
+
+        dir_path = save_dir + "/cutouts/shape_{}_{}_{}".format(max_shape[0], max_shape[1], max_shape[2])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        filepath_seg = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "seg")
+        filepath_ct = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "ct")
+        filepath_ctd = save_dir + "/cutouts/shape_{}_{}_{}/sub-{}_castellvi-cutout_{}_iso".format(max_shape[0], max_shape[1], max_shape[2], record["subject"], "ctd")
+        if os.path.isfile(filepath_seg + ".npy") and os.path.isfile(filepath_ct + ".npy") and skip_existing:
+            return record["subject"]
+        
+        #redirect stdout to silence the function
+        sys.stdout = open(os.devnull, "w")
+        
+        #Open files
+        seg_nii = record["seg_file"].open_nii()
+        ctd = record["ctd_file"].open_cdt()
+            
+        #Reorient to standard orientation
+        seg_nii.reorient_(axcodes_to=('P', 'I', 'R'), verbose = False)
+        ctd.reorient_(axcodes_to=('P', 'I', 'R'), _shape = seg_nii.shape, verbose = False)
+        ctd.zoom = seg_nii.zoom
+
+        #detach arrays to manipulate them
+        seg_arr = seg_nii.get_array()
+
+        #Compute cutouts
+        ap_slice, lr_slice, is_slice = self._compute_slice(seg_arr = seg_arr, ctd = ctd, max_shape = max_shape, zoom = seg_nii.zoom)
+        seg_cutout = seg_arr[ap_slice, is_slice, lr_slice]
+
+        #Reattach to use nifty rescale function
+        seg_nii.set_array_(seg_cutout)
+
+        #Rescale to isotropic voxel spacing
+        seg_nii.rescale_(voxel_spacing = (1,1,1))
+
+        #Detach again to manipulate arrays
+        seg_cutout = seg_nii.get_array()
+
+        #Now we pad to final size
+        seg_cutout = np.pad(seg_cutout, [(0, max_shape[i] - seg_cutout.shape[i]) for i in range(len(seg_cutout.shape))],"constant")
+        
+        #Save cutouts
+        np.save(filepath_seg, arr = seg_cutout)
+        self._np_to_nifti(seg_cutout).save(filepath_seg + ".nii.gz")
+
+        #Now we do the same for the CT 
+        ct_nii = record["raw_file"].open_nii()
+        ct_nii.reorient_(axcodes_to=('P', 'I', 'R'), verbose = False)
+        ct_arr = ct_nii.get_array()
+        ct_cutout = ct_arr[ap_slice, is_slice, lr_slice]
+        ct_nii.set_array_(ct_cutout)
+        ct_nii.rescale_(voxel_spacing = (1,1,1))
+        ct_cutout = ct_nii.get_array()
+        ct_cutout = np.pad(ct_cutout, [(0, max_shape[i] - ct_cutout.shape[i]) for i in range(len(ct_cutout.shape))],"constant")
+        np.save(filepath_ct, arr = ct_cutout)
+        self._np_to_nifti(ct_cutout).save(filepath_ct + ".nii.gz")
+
+        #Change ctd to match new shape
+        centroids = ctd.centroids
+        cutout_centroids = {key: (centroids[key][0] - ap_slice.start, centroids[key][1] - is_slice.start, centroids[key][2] - lr_slice.start) for key in centroids.keys() if key in seg_cutout}
+        #Drop all centroids with any negative value
+        cutout_centroids = {key: cutout_centroids[key] for key in cutout_centroids.keys() if not any([cutout_centroids[key][i] < 0 for i in range(len(cutout_centroids[key]))])}
+        ctd.centroids = cutout_centroids
+        ctd.rescale_(voxel_spacing = (1,1,1))
+
+        #Save centroids
+        ctd.save(filepath_ctd + ".json")
+
+        #Revert stdout
+        sys.stdout = sys.__stdout__
+
+        return record["subject"]
+
     def _max_shape_job(self, family):
-        #TODO: refactor for use with family
         """
         Args:
             list of families to handle.
@@ -310,7 +386,6 @@ class DataHandler:
         
 
     def _get_max_shape(self, multi_family_subjects, n_jobs = 8):
-        #TODO: refactor for use with family
         """
         Args:
             subject name
@@ -373,16 +448,17 @@ def read_config(config_file):
     return config
 
 def main():
-    WORKING_DIR = "/data1/practical-sose23/castellvi/3D-Castellvi-Prediction/"
+    WORKING_DIR = "/home/daniel/Documents/Uni/practical-sose23/castellvi/3D-Castellvi-Prediction/"
     logging.basicConfig(filename=WORKING_DIR + 'data/prepare_data.log', encoding='utf-8', level=logging.DEBUG)
-    dataset = [WORKING_DIR  + 'data/dataset-verse19',  WORKING_DIR + 'data/dataset-verse20', WORKING_DIR + "data/dataset-tri"]
+    dataset = [WORKING_DIR  + 'data/dataset-verse19',  WORKING_DIR + 'data/dataset-verse20', WORKING_DIR + "data/dataset-tri", WORKING_DIR + "data/dataset-tri"]
     data_types = ['rawdata',"derivatives"]
     image_types = ["ct", "subreg"]
-    master_list = WORKING_DIR + 'data/Castellvi_list.xlsx'
+    master_list = '/home/daniel/Documents/Uni/practical-sose23/castellvi/3D-Castellvi-Prediction/src/dataset/VerSe_masterlist_V4.xlsx'
     processor = DataHandler(master_list=master_list ,dataset=dataset, data_types=data_types, image_types=image_types)
-    sample = processor.tri_records[1]
     #processor._get_cutout(sample,return_seg = False, max_shape=(128, 86, 136),save_dir = WORKING_DIR + "data", skip_existing=True)
-    processor._prepare_cutouts(save_dir = WORKING_DIR + "data")
+    processor._prepare_cutouts(save_dir = WORKING_DIR + "data", n_jobs=8, skip_existing=False)
+
+
     
     
 if __name__ == "__main__":
