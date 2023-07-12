bf215f2b670adb57d3350550a26ba7c2afb95ecf
tumamur <arda.mamur@tum.de>
2023-07-12 02:47:08 +0200

    fix bug in parameter name

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -6,9 +6,69 @@ export PYTHONWARNINGS="ignore"
 export PYTHONPATH=$PYTHONPATH:/data1/practical-sose23/castellvi/castellvi_prediction/bids
 
 
+# python3 train.py \
+#         --flip_all \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --n_epochs 70 \
+#         --batch_size 16 \
+#         --learning_rate 0.0001 \
+#         --weight_decay 0.001 \
+#         --classification_type right_side_binary \
+#         --model densenet \
+#         --num_classes 2 \
+#         --rotate_range 20 \
+#         --translate_range 0.2  \
+#         --scale_range 0.8 1.2 \
+#         --aug_prob 1 \
+#         --accumulate_grad_batches 4 \
+#         --num_workers 8 \
+#         --dropout_prob 0.3 \
+
+# python3 train.py \
+#         --flip_all \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --n_epochs 70 \
+#         --batch_size 16 \
+#         --learning_rate 0.0001 \
+#         --weight_decay 0.001 \
+#         --classification_type right_side_binary \
+#         --model densenet \
+#         --num_classes 2 \
+#         --rotate_range 20 \
+#         --translate_range 0.2  \
+#         --scale_range 0.8 1.2 \
+#         --aug_prob 1 \
+#         --accumulate_grad_batches 4 \
+#         --num_workers 8 \
+#         --dropout_prob 0.1 \
+
+# python3 train.py \
+#         --flip_all \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --n_epochs 70 \
+#         --batch_size 16 \
+#         --learning_rate 0.0001 \
+#         --weight_decay 0.001 \
+#         --classification_type right_side_binary \
+#         --model densenet \
+#         --num_classes 2 \
+#         --rotate_range 20 \
+#         --translate_range 0.2  \
+#         --scale_range 0.8 1.2 \
+#         --aug_prob 1 \
+#         --accumulate_grad_batches 4 \
+#         --num_workers 8 \
+#         --elastic_transform \
+#         --dropout_prob 0.1 \
+
 python3 train.py \
         --flip_all \
         --use_zero_out \
+        --use_seg \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
@@ -28,6 +88,8 @@ python3 train.py \
 python3 train.py \
         --flip_all \
         --use_zero_out \
+        --use_seg \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
@@ -47,6 +109,8 @@ python3 train.py \
 python3 train.py \
         --flip_all \
         --use_zero_out \
+        --use_seg \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
@@ -68,71 +132,7 @@ python3 train.py \
         --flip_all \
         --use_zero_out \
         --use_seg \
-        --use_seg_binary \
-        --weighted_sample \
-        --n_epochs 70 \
-        --batch_size 16 \
-        --learning_rate 0.0001 \
-        --weight_decay 0.001 \
-        --classification_type right_side_binary \
-        --model densenet \
-        --num_classes 2 \
-        --rotate_range 20 \
-        --translate_range 0.2  \
-        --scale_range 0.8 1.2 \
-        --aug_prob 1 \
-        --accumulate_grad_batches 4 \
-        --num_workers 8 \
-        --dropout_prob 0.3 \
-
-python3 train.py \
-        --flip_all \
-        --use_zero_out \
-        --use_seg \
-        --use_seg_binary \
-        --weighted_sample \
-        --n_epochs 70 \
-        --batch_size 16 \
-        --learning_rate 0.0001 \
-        --weight_decay 0.001 \
-        --classification_type right_side_binary \
-        --model densenet \
-        --num_classes 2 \
-        --rotate_range 20 \
-        --translate_range 0.2  \
-        --scale_range 0.8 1.2 \
-        --aug_prob 1 \
-        --accumulate_grad_batches 4 \
-        --num_workers 8 \
-        --dropout_prob 0.1 \
-
-python3 train.py \
-        --flip_all \
-        --use_zero_out \
-        --use_seg \
-        --use_seg_binary \
-        --weighted_sample \
-        --n_epochs 70 \
-        --batch_size 16 \
-        --learning_rate 0.0001 \
-        --weight_decay 0.001 \
-        --classification_type right_side_binary \
-        --model densenet \
-        --num_classes 2 \
-        --rotate_range 20 \
-        --translate_range 0.2  \
-        --scale_range 0.8 1.2 \
-        --aug_prob 1 \
-        --accumulate_grad_batches 4 \
-        --num_workers 8 \
-        --elastic_transform \
-        --dropout_prob 0.1 \
-
-python3 train.py \
-        --flip_all \
-        --use_zero_out \
-        --use_seg \
-        --use_seg_binary \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
@@ -154,7 +154,7 @@ python3 train.py \
         --flip_all \
         --use_zero_out \
         --use_seg \
-        --use_seg_binary \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
@@ -176,7 +176,7 @@ python3 train.py \
         --flip_all \
         --use_zero_out \
         --use_seg \
-        --use_seg_binary \
+        --use_bin_seg \
         --weighted_sample \
         --n_epochs 70 \
         --batch_size 16 \
