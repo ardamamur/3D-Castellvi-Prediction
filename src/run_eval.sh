c3272fea83e091bfb7fd47c8e8fdc47336dd6717
tumamur <arda.mamur@tum.de>
2023-07-14 14:10:51 +0200

    latest

--- src/run_eval.sh
+++ src/run_eval.sh
@@ -6,6 +6,55 @@ export PYTHONWARNINGS="ignore"
 export PYTHONPATH=$PYTHONPATH:/data1/practical-sose23/castellvi/castellvi_prediction/bids
 
 
+# python3 eval.py \
+#         --eval_type test \
+#         --flip_all \
+#         --use_seg \
+#         --use_bin_seg \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --classification_type right_side \
+#         --model densenet \
+#         --num_classes 3 \
+#         --version_no 0
+
+# python3 eval.py \
+#         --eval_type test \
+#         --flip_all \
+#         --use_seg \
+#         --use_bin_seg \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --classification_type right_side \
+#         --model densenet \
+#         --num_classes 3 \
+#         --version_no 1
+
+# python3 eval.py \
+#         --eval_type test \
+#         --flip_all \
+#         --use_seg \
+#         --use_bin_seg \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --classification_type right_side \
+#         --model densenet \
+#         --num_classes 3 \
+#         --version_no 2
+
+# python3 eval.py \
+#         --eval_type test \
+#         --flip_all \
+#         --use_seg \
+#         --use_bin_seg \
+#         --use_zero_out \
+#         --weighted_sample \
+#         --classification_type right_side \
+#         --model densenet \
+#         --num_classes 3 \
+#         --version_no 3
+
+
 python3 eval.py \
         --eval_type test \
         --flip_all \
@@ -16,4 +65,16 @@ python3 eval.py \
         --classification_type both_side \
         --model densenet \
         --num_classes 3 \
-        --version_no 0
+        --version_no 4
+
+python3 eval.py \
+        --eval_type test \
+        --flip_all \
+        --use_seg \
+        --use_bin_seg \
+        --use_zero_out \
+        --weighted_sample \
+        --classification_type both_side \
+        --model densenet \
+        --num_classes 3 \
+        --version_no 5
